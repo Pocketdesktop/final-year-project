1ab2e1548abad0cbb99240865c2a49e4152a0d8b
shardul3196 <shardul3196@gmail.com>
2017-09-04 22:40:52 +0530

    first index.js file

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+
+app.listen(3000,function(err)
+{
+	if(err){
+		console.log(err);
+	}
+	else {
+		console.log("listening on port 3000");
+	}
+});
